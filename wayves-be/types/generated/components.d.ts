65b5d0f163e97cce04a4664aed919f67a9f6a00e
Micheal Callaghan <mehaul96@gmail.com>
2025-03-21 01:10:52 -0400

    strappi config

--- wayves-be/types/generated/components.d.ts
+++ wayves-be/types/generated/components.d.ts
@@ -1,3 +1,32 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ElementsLink extends Struct.ComponentSchema {
+  collectionName: 'components_elements_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+  };
+}
+
+export interface ElementsLogo extends Struct.ComponentSchema {
+  collectionName: 'components_elements_logos';
+  info: {
+    displayName: 'logo';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'>;
+    logoText: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'elements.link': ElementsLink;
+      'elements.logo': ElementsLogo;
+    }
+  }
+}
